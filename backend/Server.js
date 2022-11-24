125814799eefc33db90ffa5e0ecbf29a77cb0f34
Sayem <sayemrayeen068@gmail.com>
2022-11-24 14:25:24 +0530

    user authentication endpoints done

--- backend/Server.js
+++ backend/Server.js
@@ -4,24 +4,20 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 connectDB();
 const app = express();
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-// for example to get response from server
-// app.use((req, res, next) => {
-//   // console.log("Hello");
-//   console.log(req.originalUrl);
-//   next();
-// });
-
-app.use("/api/products", productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
