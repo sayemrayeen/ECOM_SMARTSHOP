58824e918c9f1da7872604dd7eb305372eef44d2
Sayem <sayemrayeen068@gmail.com>
2022-11-25 23:37:24 +0530

    user register reducer action screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/login" element={<LoginScreen />} exact />
+            <Route path="/register" element={<RegisterScreen />} exact />
             <Route path="/" element={<HomeScreen />} exact />
             <Route path="/product/:id" element={<ProductScreen />} exact />
             <Route path="/cart" element={<CartScreen />} />
