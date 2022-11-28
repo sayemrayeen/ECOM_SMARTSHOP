b19c23152feb8adf7ccd3ea2baa4cb0537a8201e
Sayem <sayemrayeen068@gmail.com>
2022-11-29 00:08:11 +0530

    Show user order on profile

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 const router = express.Router();
 import {
   addOrderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
 } from "../controller/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
-router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/myorders").get(protect, getMyOrders);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
