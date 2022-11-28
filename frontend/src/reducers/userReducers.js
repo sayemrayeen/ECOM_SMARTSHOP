ed1c2db1536de0fa7019568378fed1c4c00c2992
Sayem <sayemrayeen068@gmail.com>
2022-11-29 00:33:18 +0530

    user details and order reset added

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,8 @@
 import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
+  USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -44,14 +48,14 @@ export const userRegisterReducer = (state = {}, action) => {
 
 export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
-    case USER_REGISTER_REQUEST:
+    case USER_DETAILS_REQUEST:
       return { ...state, loading: true };
-    case USER_REGISTER_SUCCESS:
-      return { loading: false, userInfo: action.payload };
-    case USER_REGISTER_FAIL:
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
-    case USER_LOGOUT:
-      return {};
+    case USER_DETAILS_RESET:
+      return { user: {} };
     default:
       return state;
   }
