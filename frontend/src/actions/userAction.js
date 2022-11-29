fee30ec77aac74e4f14e074e28aff1cb0ce4c70e
Sayem <sayemrayeen068@gmail.com>
2022-11-29 17:40:23 +0530

    Get and Update user by ID By Admin only

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -226,11 +226,9 @@ export const deleteUser = (id) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.delete(`/api/users/${id}`, config);
+    await axios.delete(`/api/users/${id}`, config);
 
-    dispatch({
-      type: USER_DELETE_SUCCESS,
-    });
+    dispatch({ type: USER_DELETE_SUCCESS });
   } catch (error) {
     dispatch({
       type: USER_DELETE_FAIL,
